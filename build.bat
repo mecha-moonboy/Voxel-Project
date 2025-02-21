0828a03c3878f7d6c756a63c82232e9175cd0b8c
mecha_moonboy <ezshack@gmail.com>
2025-02-21 15:44:36 -0500

    Added .gitignore, cleaned up repo

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building Voxel Engine...
-cl /EHsc main.cpp Mesh.cpp Shader.cpp /I "C:\SDL2\include" /I "C:\GLEW\include" /I "C:\Voxel Project\glm" /link /LIBPATH:"C:\SDL2\lib\x86" /LIBPATH:"C:\GLEW\lib\Release\Win32" SDL2.lib SDL2main.lib shell32.lib glew32.lib opengl32.lib /SUBSYSTEM:CONSOLE
+cl /EHsc main.cpp Mesh.cpp Shader.cpp /I "C:\SDL2\include" /I "C:\GLEW\include" /I "C:\Kybus Engine\glm" /link /LIBPATH:"C:\SDL2\lib\x86" /LIBPATH:"C:\GLEW\lib\Release\Win32" SDL2.lib SDL2main.lib shell32.lib glew32.lib opengl32.lib /SUBSYSTEM:CONSOLE
 if %ERRORLEVEL% NEQ 0 (
     echo Build failed!
     pause
